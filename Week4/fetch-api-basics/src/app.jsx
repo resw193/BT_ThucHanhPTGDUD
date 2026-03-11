f1825efd62cf9e90703ec8de282b2d6f32194b00
H82M19\Student <Student>
2026-03-11 11:22:16 +0700

    add

--- Week4/fetch-api-basics/src/app.jsx
+++ Week4/fetch-api-basics/src/app.jsx
@@ -3,19 +3,35 @@ import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
 import './app.css'
 import FetchApiBasic from './components/FetchApiBasic'
+import FetchLoadingError from './components/FetchLoadingError'
 
 export function App() {
 
   return (
     <>
       {/* Bài 1 */}
-      <div>
+      <div style={{ marginTop: '40px' }}>
         <FetchApiBasic></FetchApiBasic>
       </div>
 
       {/* Bài 2 */}
-      <div>
+      <div style={{ marginTop: '80px' }}>
+        <FetchLoadingError></FetchLoadingError>
+      </div>
 
+      {/* Bài 3 */}
+      <div style={{ marginTop: '80px' }}>
+
+      </div>
+
+      {/* Bài 4 */}
+      <div style={{ marginTop: '80px' }}>
+        
+      </div>
+
+      {/* Bài 5 */}
+      <div style={{ marginTop: '80px' }}>
+        
       </div>
     </>
   )
---
+++ Week4/fetch-api-basics/src/components/FetchLoadingError.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+function FetchLoadingError() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+    useEffect(() => {
+        async function fetchdata() {
+            try {
+                setLoading(true);
+                console.log("Loading...")
+                const res = await fetch(url);
+
+                // Error 400/500 --> Catch không bắt được lỗi
+                if (!res.ok) {
+                    throw new Error("Can't fetch data from server");
+                }
+
+                var data = await res.json();
+                console.log(data);
+                setData(data);
+            } catch (error) {
+                console.log("error networking");
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchdata();
+
+    }, [])
+
+    
+
+    return (
+        <div>
+
+            <ul>
+                {data.map((user) => (
+                    <li key={user.id}>
+                        {user.name} - {user.email}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default FetchLoadingError;
